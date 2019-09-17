ed48b5da55bb820cc2dbe2f58373ab55e9545595
Hardik Priyankar <hardikpriyankar@gmail.com>
2019-09-17 17:33:29 +0530

    test request

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,19 @@
-var assert = require('assert');
-describe('test', function() {
-    it('should return an array', function() {
-        assert(Array.isArray('a,b,c'.split(',')));
-    });
-})
+const expect = require('chai').expect
+var request = require('request');
+
+
+it('Main page content', (done) => {
+    request('http://localhost:3100', (error, resp, body) => {
+        expect(body).to.equal('ci with travis');
+        done();
+    })
+});
+
+
+// const server = require('../app');
+// console.log("server ", server);
+// describe('test', () => {
+//     it('should return a string', () => {
+//         expect('ci with travis').to.equal('ci with travis');
+//     });
+// });
